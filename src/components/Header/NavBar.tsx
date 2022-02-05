68be7e0c3e5003677f6618ae659173b5da8e561f
antonio-junior <acsjunior1@gmail.com>
2022-02-04 22:17:51 -0300

    add more elements to page

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -12,6 +12,7 @@ const NavContainer = styled.nav`
 `;
 
 const CreateButton = styled.button`
+  font-family: 'Changa', sans-serif;
   background-color: #2cb2a9;
   color: #fff;
   width: 140px;
@@ -19,21 +20,44 @@ const CreateButton = styled.button`
   font-weight: bold;
   font-size: 14px;
   margin-right: 10px;
-  border-radius: 5px;
+  border-radius: 100px;
   cursor: pointer;
+  border: none;
 
   &:hover {
     background-color: #37bbb2;
   }
 `;
 
+interface BellIcon {
+  notification?: boolean;
+}
+
 const BellIcon = styled.span`
-  font-size: 23px;
+  font-size: 26px;
   background-color: #fff;
   padding: 2px;
   border-radius: 18px;
   cursor: pointer;
   transition: background-color 0.5s;
+  position: relative;
+
+  span {
+    visibility: ${(props: BellIcon) => !props.notification && 'hidden'};
+    position: absolute;
+    width: 11px;
+    border-radius: 200px;
+    background-color: #f63902;
+    top: -3px;
+    right: -2px;
+    height: 13px;
+    color: white;
+    font-size: 11px;
+    padding-right: 4px;
+    font-weight: 700;
+    padding-top: 2px;
+    border: solid 2px white;
+  }
 
   &:hover {
     background-color: #f5f5f5;
@@ -44,7 +68,8 @@ const NavBar = () => {
   return (
     <NavContainer>
       <CreateButton>Create Poll</CreateButton>
-      <BellIcon>
+      <BellIcon notification>
+        <span>1</span>
         <i className='las la-bell' />
       </BellIcon>
     </NavContainer>
--- src/components/PollList.tsx
+++ src/components/PollList.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
 
 const ListContainer = styled.div`
   font-family: 'Roboto Slab', serif;
@@ -8,26 +10,29 @@ const ListContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
 
-  table {
+const PollSetItem = styled.div`
+  padding: 12px;
+  border-radius: 20px;
+  background-color: #fff;
+  width: 80%;
+  display: flex;
+  justify-content: space-between;
+  box-shadow: 0px 1px 7px 0px #aaa;
+  flex-wrap: wrap;
+  align-items: baseline;
+
+  h4 {
     width: 80%;
-    text-align: center;
   }
+`;
 
-  tbody {
-    background-color: white;
-  }
-
-  tr {
-    line-height: 2;
-  }
-
-  thead {
-    color: white;
-    background-color: #008394;
-    font-weight: 700;
-    text-transform: uppercase;
-  }
+const PollItemMainContent = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  justify-content: space-between;
 `;
 
 interface LabelStatus {
@@ -41,103 +46,112 @@ const LabelStatus = styled.span`
     const colors = [
       { status: 'ACTIVE', color: '#a2d26e' },
       { status: 'CLOSED', color: '#fa7056' },
-      { status: 'SUSPENDED', color: '#878b8e' }
+      { status: 'PAUSED', color: '#878b8e' }
     ];
-    const [item] = colors.filter(i => i.status === props.status);
-    return item.color;
+
+    return colors.find(({ status }) => status === props.status)?.color;
   }};
 `;
 
-interface Poll {
+const ActionButton = styled.div`
+  cursor: pointer;
+`;
+
+const USER_POLLS = gql`
+  query pollsets {
+    pollsets {
+      id
+      title
+      uuid
+      status
+      allowpublic
+      dtExpiration
+      polls {
+        id
+        question
+        maxselections
+        rightanswer
+        alternatives {
+          id
+          description
+        }
+      }
+      totalAnswers
+    }
+  }
+`;
+
+interface TAlternative {
+  id: number;
+  description: string;
+}
+interface TPoll {
   id: number;
   question: string;
+  maxselections: number;
+  rightanswer: number;
+  alternatives: TAlternative[];
+}
+interface TPollSet {
+  id: number;
+  title: string;
   uuid: string;
   status: string;
-  allowPublic: boolean;
-  createdAt: string;
-  expiration: string | null;
-  totalVotes: number;
+  allowpublic: boolean;
+  dtExpiration: string;
+  totalAnswers: number;
+  polls: TPoll[];
 }
-const polls: Poll[] = [
-  {
-    id: 1,
-    question: 'Lorem ipsum dolor seti apis ?',
-    uuid: '12312-1312312-123322',
-    status: 'ACTIVE',
-    allowPublic: true,
-    createdAt: '23/10/2019',
-    expiration: '01/06/2020 21:00',
-    totalVotes: 223
-  },
-  {
-    id: 2,
-    question: 'Lorem ipsum dolor seti apis ?',
-    uuid: '12312-1312312-123322',
-    status: 'CLOSED',
-    allowPublic: false,
-    createdAt: '23/10/2019',
-    expiration: null,
-    totalVotes: 223
-  },
-  {
-    id: 3,
-    question: 'Lorem ipsum dolor seti apis ?',
-    uuid: '12312-1312312-123322',
-    status: 'SUSPENDED',
-    allowPublic: true,
-    createdAt: '23/10/2019',
-    expiration: null,
-    totalVotes: 223
-  }
-];
+interface TData {
+  pollsets: TPollSet[];
+}
 
 const PollList = () => {
+  const { loading, error, data } = useQuery<TData>(USER_POLLS);
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>{error.message}</p>;
+
   return (
     <ListContainer>
-      <table>
-        <thead>
-          <tr>
-            <td>Actions</td>
-            <td>Description</td>
-            <td>Created at</td>
-            <td>Status</td>
-            <td>Public</td>
-            <td>Expires at</td>
-            <td>Votes</td>
-          </tr>
-        </thead>
-        <tbody>
-          {polls.map(({ id, question, uuid, status, allowPublic, createdAt, expiration, totalVotes }) => (
-            <tr key={id}>
-              <td>
-                <a href='/'>
+      {data?.pollsets.map(({ id, polls, title, uuid, status, allowpublic, dtExpiration, totalAnswers }) => (
+        <PollSetItem key={id}>
+          {/* <TDAction>
+                <ActionButton>
                   <i className='las la-edit' />
-                </a>
-                <a href='/'>
+                </ActionButton>
+                <ActionButton>
                   <i className='las la-pause-circle' />
-                </a>
-                <a href='/'>
+                </ActionButton>
+                <ActionButton>
                   <i className='las la-window-close' />
-                </a>
-                <a href='/'>
-                  <i className='las la-link' />
-                </a>
-                <a href='/'>
+                </ActionButton>
+                <ActionButton>
+                  <a href={`http://www-${uuid}`} title='Go to Poll'>
+                    <i className='las la-link' />
+                  </a>
+                </ActionButton>
+                <ActionButton>
                   <i className='las la-clipboard' />
-                </a>
-              </td>
-              <td>{question}</td>
-              <td>{createdAt}</td>
-              <td>
-                <LabelStatus status={status}>{status}</LabelStatus>
-              </td>
-              <td>{String(allowPublic)}</td>
-              <td>{expiration || 'Not expires'}</td>
-              <td>{totalVotes}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                </ActionButton>
+                <ActionButton>
+                  <i className='las la-eye' />
+                </ActionButton>
+              </TDAction> */}
+          <h4>
+            {title} - {polls[0].question}
+          </h4>
+          <div>
+            <LabelStatus status={status}>{status}</LabelStatus>
+          </div>
+          <PollItemMainContent>
+            <div>Due: {dtExpiration}</div>
+
+            <div>{allowpublic ? 'PUBLIC' : 'PRIVATE'} | </div>
+            <div>{dtExpiration || '-'} |</div>
+            <div>{totalAnswers} answers</div>
+          </PollItemMainContent>
+        </PollSetItem>
+      ))}
     </ListContainer>
   );
 };
