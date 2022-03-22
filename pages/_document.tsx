a879f362352ffd7c95f1d31309053865e56d893f
Antonio Junior <acsjunior1@gmail.com>
2022-03-21 22:17:52 -0300

    setup Next and Tailwind

---
+++ pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, {DocumentContext, Html, Head, Main, NextScript} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html className="h-full bg-gray-50">
+        <Head/>
+        <body className="h-full">
+        <Main/>
+        <NextScript/>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument
