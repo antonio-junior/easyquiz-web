66040b070eb24ab0863f2c2594635a2b5ac5c482
Antonio Junior <acsjunior1@gmail.com>
2022-03-21 23:46:51 -0300

    setup prettier and eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
+/* eslint-disable */
 const { join } = require('path');
 
 module.exports = {
   content: [
-    join(__dirname, "pages/**/*.{js,ts,jsx,tsx}"),
-    join(__dirname, "components/**/*.{js,ts,jsx,tsx)"),
+    join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
+    join(__dirname, 'components/**/*.{js,ts,jsx,tsx)'),
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [],
+};
